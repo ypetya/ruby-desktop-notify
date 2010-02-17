3ab9ae45fc3aacf3c421b406ea0eddc5ace37ac3
Kiss Péter <ypetya@gmail.com>
2010-02-17 17:44:43 +0100

    empty url -> invalid

--- skype_safe_notify.rb
+++ skype_safe_notify.rb
@@ -46,7 +46,7 @@ module SkypeNotify
   NOT_VALID_URL = [ /local/, /http:\/\/[0-9\.]+[\/:]/, /private/, 
     /virgo/, /ypetya/, /admin/, /sandbox/, /szarka/, /netpincer/, 
     /blackbox/, /svn/, /authkey=\w+&/i, /iwiw/, /zoldseg/, /gtk/,
-    /eleventyone/, /deep-river/, /zanz.*itori/]
+    /eleventyone/, /deep-river/, /zanz.*itori/, /^http[s]*:\/\/\s*$/]
 
   # replace emoticons
   EMOTIES = { 
